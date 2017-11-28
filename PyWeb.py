268bd3ab22f4a7da4bf27f1c45da034ecf78bd55
LavaPower <lavapower84@gmail.com>
2017-11-28 18:57:24 +0100

    V 0.0.2 : Url Update
    - Ajout d'une barre d'url (qui se met à jour automatiquement)
    - Ajout d'un bouton pour entrer l'url
    - Ajout d'un bouton pour revenir en arrière
    - Ajout d'un bouton pour aller en avant

--- PyWeb.py
+++ PyWeb.py
@@ -1,13 +1,44 @@
 import sys
 
 from PyQt4.QtWebKit import QWebView
-from PyQt4.QtGui import QApplication
-from PyQt4.QtCore import QUrl
+from PyQt4.QtGui import QApplication, QGridLayout, QLineEdit, QWidget, QPushButton
+from PyQt4.QtCore import *
+
+class UrlInput(QLineEdit):
+    def __init__(self, browser):
+        super(UrlInput,self).__init__("http://google.com")
+        self.browser = browser
+
+    def enterUrl(self):
+        url = QUrl(self.text())
+        browser.load(url)
+
+    def setUrl(self):
+        self.setText(browser.url().toString())
 
 app = QApplication(sys.argv)
 
+grid = QGridLayout()
 browser = QWebView()
-browser.load(QUrl("http://youtube.com"))
-browser.show()
+browser.load(QUrl("http://google.com"))
+urlInput = UrlInput(browser)
+urlEnter = QPushButton("Entrer")
+back = QPushButton("<")
+forward = QPushButton(">")
+
+urlEnter.clicked.connect(urlInput.enterUrl)
+browser.urlChanged.connect(urlInput.setUrl)
+back.clicked.connect(browser.back)
+forward.clicked.connect(browser.forward)
+
+grid.addWidget(back, 0, 0)
+grid.addWidget(forward, 0, 1)
+grid.addWidget(urlInput, 0, 2)
+grid.addWidget(urlEnter, 0, 3)
+grid.addWidget(browser, 1, 0, 1, 4)
+
+main = QWidget()
+main.setLayout(grid)
+main.show()
 
 app.exec_()
