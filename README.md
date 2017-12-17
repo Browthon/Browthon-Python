a9b040ca001129208b7153f1b1d364a8b20b9fe0
LavaPower <lavapower84@gmail.com>
2017-12-17 17:44:52 +0100

    Amélioration Historique + Séparation
    - Ajout de la suppression de l'historique
    - Séparation de MainWindow dans le fichier "PyWeb_main"
    - Déplacement des fichiers .py utilisé par "PyWeb.py" dans files

--- README.md
+++ README.md
@@ -27,7 +27,9 @@ Navigateur web en python
 - Création d'une page perso à PyWeb (https://lavapower.github.io/pyweb.html)
 - Page perso comme page d'accueil par défaut.
 - Confirmation avant extinction lors de la fermeture du dernier onglet.
-- Début de l'historique (Affichage mais pas d'interaction)
+- Début de l'historique (Affichage + Suppression mais pas d'interaction)
+- Séparation de MainWindow dans le fichier "PyWeb_main"
+- Déplacement des fichiers .py utilisé par "PyWeb.py" dans files
 - Bug Fix : Toutes les lettres identiques à la première sont en majuscules.
 
 ### V 0.4.0 : Tab Update V2 (LASTEST) :
---
+++ files/PyWeb_utils.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3.6
+# coding: utf-8
+
+from PySide.QtWebKit import *
+from PySide.QtGui import *
+from PySide.QtCore import *
+
+class UrlInput(QLineEdit):
+	def __init__(self, main):
+		super(UrlInput,self).__init__(main.url)
+		self.browser = main.browser
+
+	def enterUrl(self):
+		urlT = self.text()
+		if "http://" in urlT or "https://" in urlT:
+			url = QUrl(urlT)
+		else:
+			urlT = "http://"+urlT
+			url = QUrl(urlT)
+		self.browser.load(url)
+	
+	def enterUrlGiven(self, url):
+		urlT = url
+		if "http://" in urlT or "https://" in urlT:
+			url = QUrl(urlT)
+		else:
+			urlT = "http://"+urlT
+			url = QUrl(urlT)
+		self.browser.load(url)
+
+	def setUrl(self):
+		self.setText(self.browser.url().toString())
+
+class Onglet(QWebPage):
+	def __init__(self, nb, main, button):
+		super(Onglet,self).__init__()
+		self.nb = nb
+		self.button = button
+		self.main = main
+		self.mainFrame().load(QUrl(main.url))
+			
+	def setOnglet(self):
+		self.main.browser.setPage(self)
+		self.main.urlInput.setUrl()
+		self.main.setTitle()
+
+class ButtonOnglet(QPushButton):
+	def __init__(self,main,text):
+		super(ButtonOnglet,self).__init__(text)
+		self.main = main
+
+	def showEvent(self, e):
+		for i in self.main.onglets:
+			if i[1] == self:
+				names = self.main.url.split(".")
+				nom = names[0].replace("https://","")
+				nom = nom.replace("http://","")
+				first = nom[0].upper()
+				nom = first + nom[1:]
+				
+				if len(nom)>=13:
+					titre = nom[:9]+"..."
+				else:
+					titre = nom
+				self.setText(titre) 
+
+class HistoryItem:
+	def __init__(self, main, title, url):
+		self.main = main
+		self.url = url
+		self.title = title
+	
+	def load(self):
+		print("tet")
+		self.main.urlInput.enterUrlGiven(self.url)
